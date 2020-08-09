35bd331b4ac53a82de5c699b023a90e2aa514f59
rohitmeena12 <rohit.meena@immortal-technologies.com>
2020-08-09 09:41:13 +0530

    Updates

---
+++ precache-manifest.6e085e9b0c71dde0027c90fa93da61a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6737e3b708aa31b94a405768219bcf13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748f1389ef2c62a354c1",
+    "url": "/static/js/2.dd41f195.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dd41f195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514bc96dde035638b5f8",
+    "url": "/static/js/3.d47cd08e.chunk.js"
+  },
+  {
+    "revision": "f5c8e0142f7146781a00",
+    "url": "/static/js/main.911e09c3.chunk.js"
+  },
+  {
+    "revision": "b0b6a7dd344392c270b3",
+    "url": "/static/js/runtime-main.df68765b.js"
+  }
+]);
